d36a5bc2264c3f29d43cb989614febf68b51c4cd
damiaNR10 <damianrog96@gmail.com>
2021-01-15 19:21:22 +0100

    feat: html structure made with react.createElement

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const rootElement = document.querySelector('#root');
-
 // const domElement = document.createElement("h2");
 // domElement.className = "Clock";
 // domElement.textContent = "Pozostało 13:36";
 
 //rootElement.appendChild(domElement);
 
-// const element = React.createElement(
-//     "h2",
-//     {className: "Clock",},
-//     "Pozostało 13:35",
-// );
+//Testowanie tworzenia elementów przez funkcję
 
-// ReactDOM.render([element, element], rootElement);
-
-function createDOM(type, value, className, root) {
+/*
+function createDOM(type, className, value, root) {
     const domElement = document.createElement(type);
-    domElement.className = className;
-    domElement.textContent = value;
+    if(className) domElement.classList = className;
+    if(value) domElement.textContent = value;
     root.appendChild(domElement);
 }
 
-createDOM("h2", "Pozostało 13:36 - test", "Clock", rootElement);
+createDOM("h1", "", "Kurs Reacta - Tydzien 2", rootElement);
+createDOM("hr", "", "", rootElement);
+createDOM("div", "App", "", rootElement);
+createDOM("div", "TimeboxEditor inactive", "", document.querySelector('.App'));
+
+*/
+
+const header = React.createElement(
+    "h1", "", "Kurs Reacta - Tydzien 1",
+);
+
+const app = React.createElement(
+    "div", {className: "App"}, "",
+);
+
+const TimeboxEditor = React.createElement(
+    "div", {className: "TimeboxEditor inactive"}, "",
+);
+
+const Timebox = React.createElement(
+    "div", {className: "Timebox"}, "",
+);
+
+const Label1 = React.createElement(
+    "label", {disabled: "disabled", value: "Uczę się skrótów klawiszowych", type: "text"}, "Co robisz?",
+);
+
+const Label2 = React.createElement(
+    "label", {disabled: "disabled", value: "25", type: "number"}, "Co robisz?",
+);
+
+const Button = React.createElement(
+    "button", {}, "Zacznij"
+);
+
+ReactDOM.render([header, app], rootElement);
+ReactDOM.render([TimeboxEditor, Timebox], document.querySelector('.App'));
+ReactDOM.render([Label1, Label2, Button], document.querySelector('.TimeboxEditor'));
